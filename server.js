0a7d5657222c8c1df3738063bfb0722aa5464e4d
coldblow <chiragkadam.iitkgp@gmail.com>
2021-12-18 19:02:20 +0530

    Procfile added

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+server.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Server running on port ${port}`);
+});
